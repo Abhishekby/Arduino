1c1d0ba9c61d356dc739542ba71b3a2e9f46e261
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2022-12-24 10:38:25 +0530

    Create blink_led1.ino

---
+++ blink_led1.ino
@@ -0,0 +1,14 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
