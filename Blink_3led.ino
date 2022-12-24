1f453210119729d60086583a8d0bbef8769afe19
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2022-12-24 10:52:23 +0530

    Create Blink_3led.ino

---
+++ Blink_3led.ino
@@ -0,0 +1,23 @@
+int led2=3,led3=2,led1=4;
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100); 
+  digitalWrite(3, HIGH);
+  delay(100);
+  digitalWrite(3, LOW);
+  delay(100); 
+  digitalWrite(4, HIGH);
+  delay(100);
+  digitalWrite(4, LOW);
+  delay(100); 
+}
