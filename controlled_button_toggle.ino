ecd45bc4b7c8106e1dc6477bfd6c9d9b26ca4a5a
Abhishek Yadav <57463882+Abhishekby@users.noreply.github.com>
2022-12-28 16:43:26 +0530

    Create controlled_button_toggle.ino

---
+++ controlled_button_toggle.ino
@@ -0,0 +1,25 @@
+bool led_state=false;
+unsigned long tc=0;
+
+void setup()
+{  Serial.begin(9600);
+  pinMode(2, INPUT);
+ pinMode(13,OUTPUT);
+ digitalWrite(13,led_state);
+}
+
+void loop()
+{ 
+  int prsd=digitalRead(2);
+  if(prsd==true)
+  {
+    if(millis()-tc>50)
+    {
+      led_state=!led_state;
+      digitalWrite(13,led_state);
+    }
+    tc=millis();
+  }
+  Serial.println(prsd);
+  
+}
